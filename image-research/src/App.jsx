2a7cf46129e551bd0af52f4fef1ee5e978a772ad
torinotebasaki <s24G1123DXs.chibakoudai.jp>
2025-09-22 14:21:49 +0900

    プログラムの作成(途中)

--- image-research/src/App.jsx
+++ image-research/src/App.jsx
@@ -1,17 +1,26 @@
 import React, {useState, useEffect} from 'react';
+import 
 
 function App(){
   const [query,setQuery] = useState();
   const [error, setError] = useState({ message: null, type: null });//エラーメッセージとエラー内容の一括管理に用いる
+  const [loading, setLoading] = useState(false);
 
   const handleSearch = () => {
     if (query.trim() === ''){ //queryが空なら何もしない
       setError({
         message: "キーワードが入力されていません",
-        type: empty_error
+        type: "empty_error"
       })
       console.log("エラータイプ:", "empty_error");//デバック用
     }else{
+      setLoading(true);
+
+    const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+    const url = `https:pixabay.com/api/?key=${API_KEY}&q=${query}``;
+`;
+
+    fetch(url)
 
     }
   }
@@ -26,7 +35,9 @@ function App(){
       />
       <button onClick = {handleSearch}>検索</button>
 
-      {error.message ? (
+      {loading ? (
+        <p>画像データを読み込み中</p>
+      ) : error.message ? (
         <p>{error.message}</p>
       ) : (
 
